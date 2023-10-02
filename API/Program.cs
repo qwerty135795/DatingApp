0552dddcb5f8ae0a05690f82fc4abb463c800efc
astra <kostya.ershov.15@gmail.com>
2023-10-02 23:25:04 +0300

    end3Section

--- API/Program.cs
+++ API/Program.cs
@@ -2,13 +2,15 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("Default"))); 
 
 builder.Services.AddControllers();
 var app = builder.Build();
 app.UseHttpsRedirection();
-
+app.UseCors(option => {
+    option.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+});
 app.UseAuthorization();
 
 app.MapControllers();
