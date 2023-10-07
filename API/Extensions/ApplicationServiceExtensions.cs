ea822ab058ce180588f6f1d3d4e1b55f70804dbd
astra <kostya.ershov.15@gmail.com>
2023-10-07 21:27:41 +0300

    end of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -13,6 +13,8 @@ namespace API.Extensions
             services.AddCors();
             services.AddDbContext<DataContext>(opt => opt.UseSqlite(config.GetConnectionString("Default")));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             return services;
         }
     }
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles() {
+            CreateMap<AppUser,MemberDTO>().ForMember(dist => dist.PhotoUrl, 
+            opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+            .ForMember(dist => dist.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo,PhotoDTO>();
+        }
+    }
+}
