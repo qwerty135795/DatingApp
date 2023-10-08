363602950ff622a953ab9ddef077eebfb96f8107
astra <kostya.ershov.15@gmail.com>
2023-10-08 03:43:02 +0300

    End 9 section

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,19 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   registerMode = false;
   users:any;
-  constructor(private readonly http:HttpClient) {}
+  constructor() {}
   onRegister() {
     this.registerMode = !this.registerMode;
   }
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('complete request')
-    });
-  }
   ngOnInit(): void {
-      this.getUsers();
   }
   onCancelForm(event:boolean) {
     this.registerMode = event;
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -18,8 +18,7 @@ export class NavComponent implements OnInit {
     private router:Router,private toastr:ToastrService) {}
   login() {
     this.accountService.login(this.model).subscribe({
-      next: () => this.router.navigateByUrl('/members'),
-      error: error => this.toastr.error(error.error)
+      next: () => this.router.navigateByUrl('/members')
     })
   }
   logout() {
